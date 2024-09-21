eaf2ca0ba1a5f77fa2533d64f26c1d2065b8db84
Emilio Renders <emilioxrenders@Emilios-MacBook-Air.local>
2024-09-21 18:00:48 +0200

    typo

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home({ photos, page }) {
           <title>Pexels Project</title>
         </Head>
 
-        {/* Photo list */}
+        {/* Photo List */}
         <div className="flex flex-col gap-5">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {photos.map((photo) => (
